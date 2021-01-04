2ef7d87899d0e520f9d0c01911cd8f3f9a60e3b5
tomyyyyy <1784489906@qq.com>
2021-01-04 10:55:54 +0800

    test

--- reptial/cvedetail_sql.py
+++ reptial/cvedetail_sql.py
@@ -59,9 +59,9 @@ class spider(object):
         #控制线程进度，确定能够生产完毕
         producer_thread.join()
         url_queue.join()
-        # print(url_queue.qsize())
+        print(url_queue.qsize())
         cve_info_queue.join()
-        # print(cve_info_queue.qsize())
+        print(cve_info_queue.qsize())
 
         self.conn.commit()
         # self.conn.close()
