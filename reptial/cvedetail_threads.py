448ce380b4f3cd5c94b3e871416ae00b687e1ca9
tomyyyyy <1784489906@qq.com>
2021-01-06 21:48:32 +0800

    test

--- reptial/cvedetail_threads.py
+++ reptial/cvedetail_threads.py
@@ -2,7 +2,6 @@
 import gc
 import sys
 import requests
-import xlsxwriter
 from lxml import etree
 from tqdm import tqdm
 import threading
@@ -16,7 +15,8 @@ class spider(object):
         self.thread_num = 10
         self.trytimes = 3
         self.lock = threading.Lock()
-        self.conn = sqlite3.connect('cvedetail.db')
+        self.conn = sqlite3.connect('cvedetail.db',check_same_thread=False)
+        self.conn.execute('PRAGMA synchronous = OFF')
 
     #cev_setails中按照时间找寻cve的信息
     def vulnerabilities_by_date(self,year):
@@ -29,17 +29,16 @@ class spider(object):
         link = html.xpath('//*[@id="pagingb"]/a/@href')
         page_link = ["https://www.cvedetails.com" + i for i in link]
  
+        #创建表格
+        cur = self.conn.cursor()
+        #cve_id, cve_type, cve_score, cve_authority, cve_vendor, cve_produce, cve_produce_version
+        sql = F"CREATE TABLE IF NOT EXISTS cve{year}(cve_id TEXT PRIMARY KEY, type TEXT,score TEXT, authority TEXT, vendor TEXT, produce TEXT, produce_version TEXT)"
+        cur.execute(sql)
 
-        #创建表格并写入表头
-        workbook = xlsxwriter.Workbook(F'cve_details_{year}.xlsx')
-        worksheet = workbook.add_worksheet()
-        con = ["cve编号","漏洞类型","cve威胁等级","获得的权限","供应商","产品","型号"]
-        worksheet.write_row("A1",con)
-        row = 1
 
         #设置两个队列
         url_queue = Queue(maxsize=self.thread_num*3)
-        cve_info_queue = Queue()
+        cve_info_queue = Queue(maxsize=self.thread_num*3)
 
         #生成cve详情url
         producer_thread = threading.Thread(target=self.producer, args=(url_queue, page_link))
@@ -53,27 +52,31 @@ class spider(object):
             consumer_thread.start()
 
         #将cve信息存储到表格之中
-        excel_thread = threading.Thread(target=self.write_excel, args=(cve_info_queue, worksheet, row,))
+        excel_thread = threading.Thread(target=self.write_sql, args=(cve_info_queue, cur, year,))
         excel_thread.setDaemon(True)
         excel_thread.start()
 
         #控制线程进度，确定能够生产完毕
         producer_thread.join()
         url_queue.join()
-        print(url_queue.qsize())
+        # print(url_queue.qsize())
         cve_info_queue.join()
-        print(cve_info_queue.qsize())
-        workbook.close()
+        # print(cve_info_queue.qsize())
+
+        self.conn.commit()
+        # self.conn.close()
+        print(F"{year}年cve信息全部写入成功")
 
 
     #将cve信息写入表格,然后删除内存数据
-    def write_excel(self, cve_info_queue, worksheet, row):
+    def write_sql(self, cve_info_queue, cur, year):
         while True:
                 if  not cve_info_queue.empty():
                     cve_info = cve_info_queue.get()
-                    worksheet.write_row(row, 0, cve_info)
-                    row += 1
-
+                    # print(cve_info)
+                    cve_info = [str(i) for i in cve_info]
+                    cur.execute(F"INSERT INTO cve{year} values(?,?,?,?,?,?,?)", (tuple(cve_info)))
+                    
                     cve_info_queue.task_done()
                     del cve_info
                     gc.collect()
@@ -97,44 +100,42 @@ class spider(object):
                 url = url_queue.get()
                 html = self.tyr_request(url, headers=self.headers)
                 #cve编号 
+                cve_id = html.xpath('//*[@id="cvedetails"]/h1/a/text()')[0]
+                #供应商 //*[@id="vulnprodstable"]/tbody/tr[2]/td[3]/a
                 try:
-                    cve_id = html.xpath('//*[@id="cvedetails"]/h1/a/text()')[0]
+                    cve_vendor = html.xpath('//*[@id="vulnprodstable"]/tr[2]/td[3]/a/text()')[0]
                 except:
-                    print(url + "异常")
-                #供应商 
+                    cve_vendor = " "
+                #产品  //*[@id="vulnprodstable"]/tbody/tr[2]/td[4]/a
                 try:
-                    cve_vendor = html.xpath('//*[@id="vulnversconuttable"]/tr[2]/td[1]/a/text()')[0]
+                    cve_produce = html.xpath('//*[@id="vulnprodstable"]/tr[2]/td[4]/a/text()')[0]
                 except:
-                    cve_vendor = None
-                #产品
+                    cve_produce = " "
+                #版本 //*[@id="vulnprodstable"]/tbody/tr[2]/td[5]
                 try:
-                    cve_produce = html.xpath('//*[@id="vulnversconuttable"]/tr[2]/td[2]/a/text()')[0]
+                    cve_produce_version = html.xpath('//*[@id="vulnprodstable"]/tr[2]/td[5]')[0].text.strip()
                 except:
-                    cve_produce = None
-                #版本
-                try:
-                    cve_produce_version = html.xpath('//*[@id="vulnversconuttable"]/tr[2]/td[3]')[0].text.strip()
-                except:
-                    cve_produce_version = None
+                    cve_produce_version = " "
                 
                 #cve漏洞类型 
                 try:
                     cve_type = html.xpath('//*[@id="cvssscorestable"]/tr[8]/td/span')[0].text
                 except:
-                    cve_type = None
+                    cve_type = " "
                 #cve威胁等级
                 cve_score = html.xpath('//*[@id="cvssscorestable"]/tr[1]/td/div')[0].text
                 #cve获得的权限
                 cve_authority = html.xpath('//*[@id="cvssscorestable"]/tr[7]/td/span')[0].text
                 cve_info = [cve_id, cve_type, cve_score, cve_authority, cve_vendor, cve_produce, cve_produce_version]
 
-                cve_info_queue.put(cve_info)
                 url_queue.task_done()
+                cve_info_queue.put(cve_info,block=True)
+   
 
-                #控制打印进度，防止不同进程同时打印
-                self.lock.acquire()
-                print(cve_info)
-                self.lock.release()
+                # #控制打印进度，防止不同进程同时打印
+                # self.lock.acquire()
+                # print(cve_info)
+                # self.lock.release()
 
 
             
@@ -147,16 +148,15 @@ class spider(object):
                     url = html.xpath('//*[@id="vulnslisttable"]/tr['+ str(2*i) + ']/td[2]/a/@href')[0]
                     cve_url = "https://www.cvedetails.com" + url
                 except:
-                    print("页面生产完毕")
                     break
-                while True:
-                    if not url_queue.full():
-                        url_queue.put(cve_url)
-                        break
+                url_queue.put(cve_url,block=True)
+
 
 
 if __name__ == "__main__":
     spider = spider()
-    spider.vulnerabilities_by_date(1999)
+    for i in range(1999,2000):
+        spider.vulnerabilities_by_date(i)
+    spider.conn.close()
 
     
