2bae48a09564f6156542f2436471604ea55728ff
tomyyyyy <1784489906@qq.com>
2021-01-09 00:17:09 +0800

    test

--- temp.py
+++ temp.py
@@ -1,21 +1,32 @@
 import asyncio
 import aiohttp
+from lxml import etree
+import requests
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def main():
+    for i in range(10):
+        async with aiohttp.ClientSession() as session:
+            html = etree.HTML(await fetch(session, 'http://baidu.com'))
+            print(html)
+
+async def test(url):
+    async with aiohttp.ClientSession() as client:
+        async with aiohttp.request('GET',url) as resp:
+
+            print(etree.HTML(await resp.text()))
+            # print(await )
 
 
 
-def main():
-    async def fetch(session, url):
-        async with session.get(url) as response:
-            return await response.text()
 
-    async def main():
-        for i in range(10):
-            async with aiohttp.ClientSession() as session:
-                html = await fetch(session, 'http://baidu.com')
-                print(i,">>>>>>>>",html)
+# tasks = [asyncio.ensure_future(fetch()) for _ in range(5)]
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 
-    # tasks = [asyncio.ensure_future(fetch()) for _ in range(5)]
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
 
-main()
+
