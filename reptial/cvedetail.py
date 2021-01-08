3df53359a56220ab5688ea7f59ae928053ce18cd
tomyyyyy <1784489906@qq.com>
2021-01-08 23:27:41 +0800

    test

--- reptial/cvedetail.py
+++ reptial/cvedetail.py
@@ -83,7 +83,7 @@ class spider(object):
 
         async def fetch(session, url):
             async with session.get(url) as response:
-                return await etree.HTML(str(response.content))
+                return await etree.HTML(response.text())
 
         async def main():
             async with aiohttp.ClientSession() as session:
