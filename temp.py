57716b1b829ebc4381fdb71a7815fe70b4d8fac2
tomyyyyy <1784489906@qq.com>
2021-01-08 15:29:07 +0800

    test

--- temp.py
+++ temp.py
@@ -5,7 +5,8 @@ from collections import OrderedDict
 
 for i in tqdm(range(5), desc='Total', ncols=100, ascii=' =', bar_format='{l_bar}{bar}|'):
     for i in tqdm(range(100), desc='Progress', ncols=100, ascii=' ='):
-        time.sleep(0.01)
+        # time.sleep(0.01)
+        pass
     for i in tqdm(range(100), desc='Progress', ncols=100, ascii=' ='):
         time.sleep(0.01)
     
